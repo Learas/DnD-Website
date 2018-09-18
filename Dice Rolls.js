34f40d60c92c1bca961cbeef83d549d1f835e871
Hans Eggert <hans.eggert@basaas.com>
2018-09-18 21:44:50 +0200

    Fix Dice class

--- Dice Rolls.js
+++ Dice Rolls.js
@@ -1,42 +1,32 @@
-class dice = {
-    sides: 6,
-    roll () {
-    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-    return randomNumber;
-  },
-};
+// class dice = {
+//     sides: 6,
+//     roll () {
+//     var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+//     return randomNumber;
+//   },
+// };
 
+class Dice {
+    constructor(sides) {
+        this.sides = sides;
+    }
 
+    roll() {
+        return Math.floor(Math.random() * this.sides) + 1;
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const dice = new Dice(6);
 // Prints dice roll to the page
 
 function printNumber(number) {
-    let placeholder = document.getElementById('placeholder');
+    const placeholder = document.getElementById('placeholder');
     placeholder.innerHTML = number;
 }
 
-let button = document.getElementById('button');
+const button = document.getElementById('button');
 
 button.onclick = function() {
-    let result = dice.roll();
+    const result = dice.roll();
     printNumber(result);
 };
